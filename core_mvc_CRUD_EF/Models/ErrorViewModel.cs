3bf59044255e7bda9d2d7ab5733a7834fb53a687
ananduNTDE <anandugnath@gmail.com>
2023-12-01 14:46:30 +0400

    changes

---
+++ core_mvc_CRUD_EF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace core_mvc_CRUD_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
